98a7931221e366cd2ae63b52984e9f6281e054da
cheekey0x <cheekey0x@outlook.com>
2024-04-07 18:47:47 +0800

    add: server action

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useTransition } from "react";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -17,10 +17,14 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
+import { login } from "@/actions/login";
 
 type Props = {};
 
 export function LoginForm({}: Props) {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
@@ -30,7 +34,19 @@ export function LoginForm({}: Props) {
   });
 
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log(values);
+    setError("");
+    setSuccess("");
+    startTransition(() => {
+      login(values).then((response) => {
+        if (response.success) {
+          setSuccess(response.message);
+          setError("");
+        } else {
+          setError(response.message);
+          setSuccess("");
+        }
+      });
+    });
   };
 
   return (
@@ -42,8 +58,8 @@ export function LoginForm({}: Props) {
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormError message="" />
-          <FormSuccess message="" />
+          <FormError message={error} />
+          <FormSuccess message={success} />
           <div className="space-y-4">
             <FormField
               control={form.control}
@@ -52,7 +68,11 @@ export function LoginForm({}: Props) {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="adi@example.com" />
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="adi@example.com"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -65,7 +85,12 @@ export function LoginForm({}: Props) {
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="*******" type="password" />
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="*******"
+                      type="password"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -73,7 +98,7 @@ export function LoginForm({}: Props) {
             />
           </div>
 
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full" disabled={isPending}>
             Login
           </Button>
         </form>
